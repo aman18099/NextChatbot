7f0441c82333839d2f18b040d8014d65afe9e720
aman <ananda.1899@gmail.com>
2025-06-03 13:21:05 +0530

    Allow Origin

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -26,7 +26,7 @@ export default function DashboardPage() {
   async function getToken() {
     const session = await supabase.auth.getSession();
     const token = session?.data?.session?.access_token;
-    console.log("Supabase token:", token);
+    // console.log("Supabase token:", token);
     return token;
   }
 
@@ -97,10 +97,11 @@ export default function DashboardPage() {
     setInput("");
     setIsTyping(true);
     try {
+      const token = await getToken();
       const res = await fetch("https://nextchatbot-6631.onrender.com/api/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json", 
-          "Authorization": `Bearer ${getToken()}`
+          "Authorization": `Bearer ${token}`
         },
         body: JSON.stringify({ question, user_id: userId }),
       });
