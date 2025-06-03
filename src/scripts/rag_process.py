f831e1d62239e9cbb7ee9202943b6441fa833b28
aman <ananda.1899@gmail.com>
2025-06-03 13:57:30 +0530

    Allow Origincors

--- src/scripts/rag_process.py
+++ src/scripts/rag_process.py
@@ -39,7 +39,7 @@ logger.addHandler(console_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
